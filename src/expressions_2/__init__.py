1440685798d5afd131417b4a0d75e5d88391c278
Rubén <rubgij@gmail.com>
2025-08-24 18:17:35 +0200

    Refactoring

--- src/expressions_2/__init__.py
+++ src/expressions_2/__init__.py
@@ -1,6 +1,6 @@
 from .basic import Expresion, Constant, Variable, Negation, Conjunction, Sum, Subtraction, Product, Division
 from .hyperbolic import Cosh, Sinh, Tanh
 from .trigonometric import Cos, Sin, Tan
-from .logarithm import Logarithm
+from .exponential import Logarithm, Exponential
 from .polynomial import Polynomial
 from .power import Power
